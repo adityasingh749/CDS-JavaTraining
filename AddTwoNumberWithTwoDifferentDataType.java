254a3dcd4f6523939e0ffe455c665ebc7d623fbc
adityasingh13102 <167447030+adityasingh13102@users.noreply.github.com>
2024-07-22 16:01:55 +0530

    TernaryOperator

--- AddTwoNumberWithTwoDifferentDataType.java
+++ AddTwoNumberWithTwoDifferentDataType.java
@@ -1,9 +1,8 @@
-public class Addition {
+public class AddTwoNumberWithTwoDifferentDataType {
     public static void main(String[] args) {
         int a = 5;
         float b = 7.5f;
 
         System.out.println("" + (a + b));
     }
-
 }
---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        String result = (age >= 18) ? "eligible" : "not eligible";
+        System.out.println(result);
+    }
+
+}
