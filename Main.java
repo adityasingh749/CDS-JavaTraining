63ce6b8fafb43a4fbbd73ca87f2e85aec785fddc
adityasingh13102 <167447030+adityasingh13102@users.noreply.github.com>
2024-07-24 11:15:02 +0530

    Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your age ");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        String result = (age >= 18) ? "eligible" : "not eligible";
+        System.out.println(result);
+
+    }
+}
