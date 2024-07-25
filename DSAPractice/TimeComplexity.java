c6c650157f26eb920d3752b922eaf2f446f1c08b
adityasingh13102 <167447030+adityasingh13102@users.noreply.github.com>
2024-07-25 15:28:16 +0530

    Time Complexity program add

--- DSAPractice/TimeComplexity.java
+++ DSAPractice/TimeComplexity.java
@@ -2,6 +2,9 @@ package DSAPractice;
 
 public class TimeComplexity {
     public static void main(String[] args) {
-
+        int i, n = 8;
+        for (i = 2; i <= n; i=(int)Math.pow(i,2)) {
+            System.out.println("Hello World !!!");
+        }
     }
 }
